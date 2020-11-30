83b60d45d953ea9d5ff08b264e61d45f36da1585
jlynch25 <38656343+jlynch25@users.noreply.github.com>
2020-11-30 20:38:09 +0000

    Partition Data into training and testing

--- readData.py
+++ readData.py
@@ -1,6 +1,14 @@
-#Importing required packages
+# Importing required packages
 import pandas as pd
+import numpy as np
 
-#Loading Data
+# Loading Data
 beer = pd.read_csv('./data/beer.txt', sep ='\t')
 beer.info()
+
+# Partition Data into training and testing
+msk = np.random.rand(len(beer)) < 0.66
+beer_train = beer[msk]
+beer_test = beer[~msk]
+print('beer_train lentgh: ', len(beer_train))
+print('beer_test lentgh: ', len(beer_test))
